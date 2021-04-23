ac172958795181d6171b29a0be9939f36e56c689
Yarb <jstrandman@gmail.com>
2021-04-23 21:46:35 +0300

    Conversion to thread and new features
    tcontroller in now a thread and commands are inserted into a queue.
    Controlled execution of sequences of commands now possible.

--- tcontroller.py
+++ tcontroller.py
@@ -2,10 +2,12 @@ import pyvjoy
 import threading
 import time
 
+READY = 1
+EXEC_UNTIL_EMPTY = 2
 
-class tController():
+class tController(threading.Thread):
 
-
+    
     def __init__(self, buttons : int, cmds: dict, holdtime, holdtime_long):
         if isinstance(buttons, int):
             self.buttons = ["0" for i in range(buttons)]
@@ -15,16 +17,24 @@ class tController():
             self.cmds = cmds
         else:
             raise typeError("cmdlist argument needs to be a list")
-
+        
+        threading.Thread.__init__(self)
         self.timers = list(range(buttons))
         self.joy1 = pyvjoy.VJoyDevice(1)
         self.holdtime = holdtime
         self.holdtime_long = holdtime_long
         
         self.lock = threading.Lock()
+        self.condition = threading.Condition()
+        
+        self.status = EXEC_UNTIL_EMPTY
+        self.sequence_exec = False
+        self.sequences = []
+        self.queue = []
 
 
-    def update_joystick(self, idx, value):
+
+    def __update_joystick(self, idx, value):
         self.lock.acquire()
         
         self.buttons[idx] = str(value)
@@ -39,35 +49,122 @@ class tController():
         
         self.lock.release()
                 
+
+    def run(self):
+        """Main loop"""
+        
+        execute = 0
+        while(True):
+            # Check status and if necessary wait for commands from queue/sequence
+            with self.condition:
+                if self.status == READY:
+                    self.status = EXEC_UNTIL_EMPTY
+                    execute = 1
+                else:
+                    self.condition.wait()
+            
+            # If execute is set, there should be data. Check and execute
+            if execute:
+                execute = 0
+                # Always ensure that queued commands are executed first.
+                while self.queue:
+                    cmd = self.queue.pop(0)
+                    self.__button_press(cmd)
+            
+                # Check if we need to execute sequences.
+                if self.sequence_exec:
+                    while self.sequences:
+                        seq = self.sequences.pop(0)
+                        for cmd in seq:
+                            self.__ordered_button_press(cmd)
+            
                 
-    def press(self, cmd):
+    def __button_press(self, cmd):
+        """Presses the button matching the given command.
+        Will launch a timer thread to automatically release the button.
+        """
+    
+        if self.__press(cmd):
+            idx = self.cmds[cmd]
+            if len(cmd) == 1:
+                holdtime = self.holdtime
+            else:
+                holdtime = self.holdtime_long
+            self.timers[idx] = threading.Timer(holdtime, self.__release, args=(cmd,))
+            self.timers[idx].start()
+
+
+    def __ordered_button_press(self, cmd):
+        """Executes one complete buttonpress. 
+        This blocks until the press/release action is completed.
+        Intended for ensuring the order of execution"""
+        
+        if self.__press(cmd):
+            if len(cmd) == 1:
+                holdtime = self.holdtime
+            else:
+                holdtime = self.holdtime_long
+            time.sleep(holdtime)
+            self.__release(cmd)
+        
+        
+    def __press(self, cmd):
+        try:
+            idx = self.cmds[cmd]
+        except KeyError:
+            raise KeyError("Given command not listed.")
+        else: 
+            if self.timers[idx] != idx:
+                self.timers[idx].cancel()
+            self.__update_joystick(idx, 1)
+            return True
+
+
+    def __release(self, cmd):
         try:
             idx = self.cmds[cmd]
         except KeyError:
             raise KeyError("Given command not listed.")
         else:
-            if self.timers[idx] != idx:
-                self.timers[idx].cancel()
-                
-            self.update_joystick(idx, 1)
-            
-            if len(cmd) == 1:
-                holdtime = self.holdtime
-            else:
-                holdtime = self.holdtime_long
-            self.timers[idx] = threading.Timer(holdtime, self.release, args=(cmd,))
-            self.timers[idx].start()
-
-
-    def release(self, cmd):
-        try:
-            idx = self.cmds[cmd]
-            self.update_joystick(idx, 0)
-        except KeyError:
-            raise KeyError("Given command not listed.")
+            self.__update_joystick(idx, 0)
+            return True
 
 
     def reset(self):
-        "tController:  Doing controller reset...."
+        """Resets timers and releases buttons"""
+
+        print("tController:  Doing controller reset....")
+        # Cancel all timers
+        for idx, timer in enumerate(self.timers):
+            if timer != idx:
+                timer.cancel()
+        # Release all buttons
         for command in self.cmds.keys():
-            self.release(command)
+            self.__release(command)
+            
+            
+    def queue_command(self, cmd):
+        """Enter the given command to execution queue"""
+        
+        self.queue.append(cmd)
+        with self.condition:
+            self.status = READY
+            self.condition.notify()
+        
+    
+    def queue_sequence(self, seq):
+        """Enter the given sequence to sequence queue"""
+
+        self.sequences.append(seq)
+        with self.condition:
+            self.status = READY
+            self.condition.notify()
+            
+            
+    def set_sequential_mode(self):
+        self.sequence_exec = True
+        
+    
+    def set_normal_mode(self):
+        self.sequence_exec = False
+        self.sequences = []
